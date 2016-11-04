3f2a1aea9c89466fa00c4b6aea0625bbdb84e7a1
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-11-04 18:08:10 -0400

    getting albums working for api

--- db/migrate/20161104214050_create_albums.rb
+++ db/migrate/20161104214050_create_albums.rb
@@ -1,7 +1,6 @@
 class CreateAlbums < ActiveRecord::Migration[5.0]
   def change
     create_table :albums do |t|
-      t.integer :id
       t.string :name
       t.string :artist_name
       t.string :cover_photo_url
