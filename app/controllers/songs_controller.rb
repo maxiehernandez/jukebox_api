639e0610d8082b4bcf1c19e1c1493897de37fc12
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-11-06 22:28:37 -0500

    added favorite attribute to json

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     songs = Album.find(params[:album_id]).songs.all
-    render json: songs, :only => [:id, :album_id, :song_name, :song_order, :song_label, :song_duration], content_type: 'application/json'
+    render json: songs, :only => [:id, :album_id, :song_name, :song_order, :song_label, :song_duration, :favorite], content_type: 'application/json'
   end
 
   def favorite
